479919e5c8d66dbc1191f5e0cc9d142e0a0b3602
erdanilevicius <steamsvabas@gmail.com>
2022-05-04 23:59:30 +0300

    first commit

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <div class="container w-full px-5 py-6 mx-auto">
+        <h1>Dėkojame, kad naudojatės mūsų paslaugomis.</h1>
+        <p>Jūsų staliuko rezervacija užregistruota.</p>
+    </div>
+</x-guest-layout>
